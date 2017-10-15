f3acf79b65cb968cd629f872cb098e9cd637ecc0
puku0x <puku0x@gmail.com>
2017-10-16 01:13:02 +0900

    fix

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,32 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { OnsenModule } from 'ngx-onsenui';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        OnsenModule,
+        CoreModule.forRoot()
+      ],
+      declarations: [
+        AppComponent
+      ],
+      providers: [],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+    }).compileComponents();
+  }));
+
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+
+});
