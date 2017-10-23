8e47ddaf341b5abc1a818ec959ebbf481e0c99e9
puku0x <puku0x@gmail.com>
2017-10-23 14:46:16 +0900

    carousel

--- src/app/pages/carousel/carousel.component.ts
+++ src/app/pages/carousel/carousel.component.ts
@@ -7,15 +7,45 @@ import * as ons from 'onsenui';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+  carouselIndex = 0;
+  items = [
+    { name: 'gray',   color: 'gray' },
+    { name: 'blue',   color: '#085078' },
+    { name: 'dark',   color: '#373B44' },
+    { name: 'orange', color: '#D38312' }
+  ];
+
   /**
    * Android
    */
   md = ons.platform.isAndroid();
 
   /**
+   * Carousel
+   */
+  @ViewChild('carousel') carousel;
+
+  /**
    * Constructor
    */
   constructor() { }
+
+  /**
+   * Callback for postchange
+   * @param event
+   */
+  onPostchange(event) {
+    this.carouselIndex = event.activeIndex;
+  }
+
+  /**
+   * Change carousel
+   * @param index
+   */
+  setActiveIndex(index) {
+    this.carouselIndex = index;
+    this.carousel.nativeElement.setActiveIndex(index);
+  }
 
   /**
    * Initialize
